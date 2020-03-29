c064f312f8c972e867cc5cddb365b158716f8fc8
Phr3nic <47946595+Phr3nic@users.noreply.github.com>
2020-03-29 19:17:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function noMoreGlobalTrade(mod) {
 	mod.hook('S_CHAT', 3, e => {
 		if (!mod.settings.enabled) return;
 		if (e.channel != 27) return;
-		return /(wts|wtb|wtt)/g.test(e.message.toLowerCase());
+		return !/(wts|wtb|wtt)/g.test(e.message.toLowerCase());
 	});
 
 	mod.command.add('globaltrade', (args) => {
